5886b79b369ce38902acfe45d22005344f36d822
Chux-Dennis <134112520+Chux-Dennis@users.noreply.github.com>
2024-11-06 07:10:25 +0100

    Solved Redundacy issue

--- src/components/manager/containers/department-store/ViewDepartmentStore.jsx
+++ src/components/manager/containers/department-store/ViewDepartmentStore.jsx
@@ -1,7 +1,7 @@
 import toast, { Toaster } from "react-hot-toast";
 import { refractor } from "../../../date";
 import React, { useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
+
 import {
   Separator,
   Grid,
